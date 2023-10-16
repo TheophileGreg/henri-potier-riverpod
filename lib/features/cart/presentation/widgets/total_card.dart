fc722f4f384aaf4ddce010a86db9ce533923ccae
Theophile <theophile@Mac-mini-de-Theophile.local>
2023-10-16 23:18:50 +0200

    add shimmer

--- lib/features/cart/presentation/widgets/total_card.dart
+++ lib/features/cart/presentation/widgets/total_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:henri_potier_riverpod/commons/widgets/shimmer_text.dart';
 import 'package:henri_potier_riverpod/features/cart/domain/entities/cart_state.dart';
 
 class TotalCard extends StatelessWidget {
@@ -28,34 +29,56 @@ class TotalCard extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 8),
-            cart.isLoading
-                ? const LinearProgressIndicator()
-                : Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Remise: \$${cart.rebate}',
-                            style: const TextStyle(fontSize: 16),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Remise: ',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        cart.isLoading
+                            ? shimmerPlaceholder(
+                                context,
+                                'chargement...',
+                              )
+                            : Text('\$${cart.rebate}')
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Nouveau prix total: ',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 12),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Nouveau prix total: \$${cart.finalPrice}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        cart.isLoading
+                            ? shimmerPlaceholder(
+                                context,
+                                'chargement...',
+                              )
+                            : Text('\$${cart.finalPrice}',
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ))
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
