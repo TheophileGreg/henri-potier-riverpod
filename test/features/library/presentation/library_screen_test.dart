3f1e307bb78b931e858c91f916b6d6eb656d2572
Theophile <theophile@Mac-mini-de-Theophile.local>
2023-10-17 16:46:34 +0200

    widget test almost perfect

--- test/features/library/presentation/library_screen_test.dart
+++ test/features/library/presentation/library_screen_test.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
@@ -19,24 +16,16 @@ import 'library_screen_test.mocks.dart';
 @GenerateMocks([
   CartNotifier,
   GetBooksUseCase,
-  HttpClient,
-  HttpClientRequest,
-  HttpClientResponse,
 ])
 void main() {
   late CartNotifier mockCartNotifier;
   late GetBooksUseCase mockGetBooksUseCase;
-  late HttpClient mockHttpclient;
-  late HttpClientRequest mockHttpclientRequest;
-  late HttpClientResponse mockHttpclientResponse;
 
   setUp(() {
     GetIt.instance.reset();
-    mockCartNotifier = MockCartNotifier();
+
     mockGetBooksUseCase = MockGetBooksUseCase();
-    mockHttpclient = MockHttpClient();
-    mockHttpclientRequest = MockHttpClientRequest();
-    mockHttpclientResponse = MockHttpClientResponse();
+    mockCartNotifier = MockCartNotifier();
 
     GetIt.instance.registerSingleton<CartNotifier>(mockCartNotifier);
   });
@@ -54,58 +43,30 @@ void main() {
     when(mockCartNotifier.state)
         .thenReturn(const CartState(false, 0, 0, 0, cart: {}));
 
-    when(mockHttpclient.getUrl(Uri.parse('https://example.com/cover.jpg')))
-        // .thenThrow(Exception());
-        .thenAnswer((_) => Future.value(mockHttpclientRequest));
-
-    when(mockHttpclientRequest.close())
-        .thenAnswer((_) => Future.value(mockHttpclientResponse));
-
-    when(mockHttpclientResponse.statusCode).thenReturn(200);
-
-    when(mockHttpclientResponse.contentLength).thenReturn(100);
-
-    when(mockHttpclientResponse.compressionState)
-        .thenReturn(HttpClientResponseCompressionState.notCompressed);
-
-    when(mockHttpclientResponse.listen(
-      any,
-      onDone: anyNamed('onDone'),
-      onError: anyNamed('onError'),
-      cancelOnError: anyNamed('cancelOnError'),
-    )).thenAnswer((Invocation invocation) {
-      final onData =
-          invocation.positionalArguments[0] as void Function(List<int>);
-
-      return Stream.value(const [1, 2, 3]).listen(onData);
-    });
-
-    await HttpOverrides.runZoned(() async {
-      await tester.pumpWidget(
-        ProviderScope(
-          overrides: [
-            cartProvider.overrideWith((ref) => mockCartNotifier),
-            libraryProvider.overrideWith((ref) async {
-              final usecase = mockGetBooksUseCase;
-              final books = await usecase();
-              return books;
-            })
-          ],
-          child: const MaterialApp(
-            home: LibraryScreen(),
-          ),
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          cartProvider.overrideWith((ref) => mockCartNotifier),
+          libraryProvider.overrideWith((ref) async {
+            final usecase = mockGetBooksUseCase;
+            final books = await usecase();
+            return books;
+          })
+        ],
+        child: const MaterialApp(
+          home: LibraryScreen(),
         ),
-      );
+      ),
+    );
 
-      await tester.pump(const Duration(seconds: 2));
-      final addButton = find.byKey(Key('add_to_cart_button_${testBook.isbn}'));
-      expect(addButton, findsOneWidget);
+    await tester.pump(const Duration(seconds: 2));
+    final addButton = find.byKey(Key('add_to_cart_button_${testBook.isbn}'));
+    expect(addButton, findsOneWidget);
 
-      await tester.tap(addButton);
-      await tester.pumpAndSettle();
+    await tester.tap(addButton);
+    await tester.pumpAndSettle();
 
-      verify(mockCartNotifier.addBookToCart(testBook)).called(1);
-    }, createHttpClient: (_) => mockHttpclient);
+    verify(mockCartNotifier.addBookToCart(testBook)).called(1);
   });
 
   testWidgets('LibraryScreen meets androidTapTargetGuideline',
